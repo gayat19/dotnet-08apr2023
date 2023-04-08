222ac9c2e36219d3262fc647afebfdf891be2a66
Gayathri Mahadevan <gayat_19@yahoo.com>
2023-04-08 12:54:35 +0530

    Day1-End

---
+++ UnderstandingTypeSaftyApp/UnderstandingTypeSaftyApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace UnderstandingTypeSaftyApp
+{
+    internal class Program
+    {
+        static void UnderstandingCollection()
+        {
+            ArrayList list = new ArrayList();
+            list.Add(10);
+            list.Add(12.4f);
+            list.Add("sfhsdj");
+            list.Add(new { Id = 101, Nam = "Ramu" });
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        static void UnderstandingGenericList()
+        {
+            List<int> list = new List<int>();   
+            list.Add(10);
+            list.Add(45);
+            list.Add(78);
+            list.Add(12);
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            UnderstandingGenericList();
+        }
+    }
+}
